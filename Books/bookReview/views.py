43461ece216947de0268aa9c763305b411adf3e7
Dhruvi <dhruvi.modi1112@gmail.com>
2023-01-30 20:46:29 +0530

    Carousel modified

--- Books/bookReview/views.py
+++ Books/bookReview/views.py
@@ -8,11 +8,22 @@ def index(request):
     # run the folowing two lines and for loop one by one in shell and print the variables to get the idea of what is happening
     category_books = Book.objects.values('category', 'book_id')
     cats = {item['category'] for item in category_books}
+
+    # Display books by subcategory
+    # subcategory_books = Book.objects.values('subcategory', 'book_id')
+    # sub_cats = {item['subcategory'] for item in subcategory_books}
+
     for cat in cats:
         product = Book.objects.filter(category=cat)
         n = len(product)
-        nSlides = n//4 + ceil((n/4) - (n//4))
+        nSlides = n//6 + ceil((n/6) - (n//6))
         allBooks.append([product, range(1, nSlides), nSlides])
+
+    # for sub_cat in sub_cats:
+    #     product = Book.objects.filter(subcategory=sub_cat)
+    #     n = len(product)
+    #     nSlides = n//4 + ceil((n/4) - (n//4))
+    #     allBooks.append([product, range(1, nSlides), nSlides])
     params={'allBooks':allBooks }
     # print(allBooks)
     return render(request, "bookReview/index.html", params)
