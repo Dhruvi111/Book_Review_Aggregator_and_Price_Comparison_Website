9dd2f9cc09c139fae1d6719f0c6bd557ebf8a1f6
Dhruvi <dhruvi.modi1112@gmail.com>
2023-02-27 21:06:41 +0530

    working on reviews

--- Books/bookReview/urls.py
+++ Books/bookReview/urls.py
@@ -12,5 +12,6 @@ urlpatterns = [
     path('signup/', views.signup, name="Signup"),
     path('details/', views.details, name="Details"),
     path('detailsHome/', views.detailsHome, name="DetailsHome"),
-    path('page/<int:digit>', views.pages, name="page")
+    path('page/<int:digit>', views.pages, name="page"),      #pagination
+    path('reviewBNoble/', views.BNoble, name="BNoble")
 ]
